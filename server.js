cd57443c14383f04dddba4e26667cfe9997fe5f8
varunswing <varuns939@gmail.com>
2020-03-29 22:05:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
 	res.json({ msg: 'Welcome to the ContactKeeper API...' })
 );
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 

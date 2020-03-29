b804c2f4be3affb319a666ff5a58a2c2ea044579
varunswing <varuns939@gmail.com>
2020-03-29 22:05:52 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route      POST api/auth
 // @desc       Register a user
 // @access     Private
-router.post('/', (req, res) => {
-    res.send('Register a user');
-});
+router.post(
+	'/',
+	[
+		check('name', 'Please add a name').not().isEmpty(),
+		check('email', 'Please enter a valid email').isEmail(),
+		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if(!errors.isEmpty()){
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('passed');
+	}
+);
 
 module.exports = router;

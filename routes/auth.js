3100e789585457bcb32c6ae4ac52d4d7b9b5ed17
varunswing <varuns939@gmail.com>
2020-04-02 23:27:20 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth')
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -10,8 +11,14 @@ const User = require('../models/User');
 // @route      GET api/auth
 // @desc       Get logged in user
 // @access     Private
-router.get('/', (req, res) => {
-	res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await  User.findById(req.user.id).select('-password');
+        res.json(user);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route      POST api/auth

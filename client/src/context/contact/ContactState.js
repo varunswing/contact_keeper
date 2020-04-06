7db65b7b815dd7f1602c5d72e2d3d361c04954ca
varunswing <varuns939@gmail.com>
2020-04-06 22:03:29 +0530

    Update ContactState.js

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,12 +1,11 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
 	ADD_CONTACT,
 	DELETE_CONTACT,
-	SET_CONTACT,
-	CLEAR_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
 	UPDATE_CONTACT,
 	FILTER_CONTACTS,
 	CLEAR_FILTER
@@ -42,12 +41,24 @@ const ContactState = props => {
 	const [state, dispatch] = useReducer(contactReducer, initialState);
 
 	// Add Contact
+	const addContact = contact => {
+		dispatch({ type: ADD_CONTACT, payload: contact });
+	};
 
 	// Delete Contact
+	const deleteContact = id => {
+		dispatch({ type: DELETE_CONTACT, payload: id});
+	};
 
 	// Set Current Contact
+	const setCurrent = contact => {
+		dispatch({ type: SET_CURRENT, payload: contact });
+	};
 
 	// Clear Current Contact
+	const clearCurrent = contact => {
+		dispatch({ type: CLEAR_CURRENT });
+	};
 
 	// Update Contact
 
@@ -58,11 +69,16 @@ const ContactState = props => {
 	return (
 		<ContactContext.Provider
 			value={{
-				contacts: state.contacts
+				contacts: state.contacts,
+				current: state.current,
+				addContact,
+				deleteContact,
+				setCurrent,
+				clearCurrent
 			}}
 		>
-            {props.children}
-        </ContactContext.Provider>
+			{props.children}
+		</ContactContext.Provider>
 	);
 };
 

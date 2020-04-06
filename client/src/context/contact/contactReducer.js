0651c092f7c7bc70b91dff6f7a9696db44479c1c
varunswing <varuns939@gmail.com>
2020-04-06 22:03:24 +0530

    Update contactReducer.js

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,21 +1,38 @@
 import {
 	ADD_CONTACT,
 	DELETE_CONTACT,
-	SET_CONTACT,
-	CLEAR_CONTACT,
+	SET_CURRENT,
+	CLEAR_CURRENT,
 	UPDATE_CONTACT,
 	FILTER_CONTACTS,
 	CLEAR_FILTER
 } from '../types';
 
-export default (state, action) =>  {
-    switch (action.type){
-        case ADD_CONTACT:
-            return {
-                ...state,
-                contacts: [...state.contacts, action.payload]
+export default (state, action) => {
+	switch (action.type) {
+		case ADD_CONTACT:
+			return {
+				...state,
+				contacts: [...state.contacts, action.payload]
+			};
+		case DELETE_CONTACT:
+			return {
+				...state,
+				contacts: state.contacts.filter(
+					contact => contact.id !== action.payload
+				)
             };
-        default:
-            return state;
-    }
+            case SET_CURRENT:
+			return {
+				...state,
+				current: action.payload
+            };
+            case CLEAR_CURRENT:
+			return {
+				...state,
+				current: null
+			};
+		default:
+			return state;
+	}
 };

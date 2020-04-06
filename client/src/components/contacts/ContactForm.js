2d3eed9155af39b5f4fe165460af4a31910496ba
varunswing <varuns939@gmail.com>
2020-04-06 22:03:12 +0530

    Update ContactForm.js

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactForm = () => {
+    const contactContext = useContext(ContactContext);
+
 	const [contact, setContact] = useState({
 		name: '',
 		email: '',
@@ -10,32 +13,68 @@ const ContactForm = () => {
 
 	const { name, email, phone, type } = contact;
 
+	const onChange = e =>
+        setContact({ ...contact, [e.target.name]: e.target.value });
+        
+    const onSubmit = e => {
+        e.preventDefault();
+        contactContext.addContact(contact);
+        setContact({
+            name: '',
+            email: '',
+            phone: '',
+            type: 'personal'
+        });
+    };
+
 	return (
-		<form>
+		<form onSubmit={onsubmit}>
 			<h2 className='text-primary'>Add Contact</h2>
 			<input
 				type='text'
-				placeholder='name'
+				placeholder='Name'
 				name='name'
 				value={name}
 				onChange={onChange}
 			/>
-            <input
+			<input
 				type='email'
 				placeholder='Email'
 				name='email'
 				value={email}
 				onChange={onChange}
 			/>
-            <input
+			<input
 				type='text'
-				placeholder='phone'
+				placeholder='Phone'
 				name='phone'
 				value={phone}
 				onChange={onChange}
 			/>
-            <h5>Contact Type</h5>
-            <input type='radio' name='type' value='personal' checked={}
+			<h5>Contact Type</h5>
+			<input
+				type='radio'
+				name='type'
+				value='personal'
+				checked={type === 'personal'}
+				onChange={onChange}
+			/>{' '}
+			Personal{' '}
+			<input
+				type='radio'
+				name='type'
+				value='professional'
+				checked={type === 'professional'}
+				onChange={onChange}
+			/>{' '}
+			Professional{' '}
+			<div>
+				<input
+					type='submit'
+					value='Add Contact'
+					className='btn btn-primary btn-block'
+				/>
+			</div>
 		</form>
 	);
 };

9c35f22096bdec41e63fdb4f46262261b5f620b9
varunswing <varuns939@gmail.com>
2020-04-26 23:19:34 +0530

    Update ContactState.js

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -36,7 +36,8 @@ const ContactState = props => {
 				type: 'professional'
 			}
 		],
-		current: null
+		current: null,
+		filtered: null
 	};
 
 	const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -67,19 +68,28 @@ const ContactState = props => {
 	}
 
 	// Filter Contacts
+	const filterContacts = text => {
+		dispatch({ type: FILTER_CONTACTS, payload: text });
+	}
 
 	// Clear Filter
+	const clearFilter = contact => {
+		dispatch({ type: CLEAR_FILTER });
+	};
 
 	return (
 		<ContactContext.Provider
 			value={{
 				contacts: state.contacts,
 				current: state.current,
+				filtered: state.filtered,
 				addContact,
 				deleteContact,
 				setCurrent,
 				clearCurrent,
-				updateContact
+				updateContact,
+				filterContacts,
+				clearFilter
 			}}
 		>
 			{props.children}

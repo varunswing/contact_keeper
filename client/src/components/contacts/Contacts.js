47aad4ca80083d4d308480cedd61e3b8a8471141
varunswing <varuns939@gmail.com>
2020-04-26 23:19:17 +0530

    Update Contacts.js

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,27 @@
-import React, { Fragment, useContext} from 'react';
+import React, { Fragment, useContext } from 'react';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
-    const contactContext = useContext(ContactContext);
+	const contactContext = useContext(ContactContext);
 
-    const { contacts } = contactContext;
+	const { contacts, filtered } = contactContext;
 
-    return(
-        <Fragment>
-            {contacts.map(contact => (
-                <ContactItem key={contact.id} contact={contact} />
-            ))}
-        </Fragment>
-    );
+	if (contacts.length === 0) {
+		return <h4>Please add a contact</h4>;
+	}
+
+	return (
+		<Fragment>
+			{filtered !== null
+				? filtered.map(contact => (
+						<ContactItem key={contact.id} contact={contact} />
+				  ))
+				: contacts.map(contact => (
+						<ContactItem key={contact.id} contact={contact} />
+				  ))}
+		</Fragment>
+	);
 };
 
 export default Contacts;

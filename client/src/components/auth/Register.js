3d803cd0dceb2df972f65a05c1de7344fb16c7f9
varunswing <varuns939@gmail.com>
2020-04-30 23:42:16 +0530

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,14 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
 import AuthContext from '../../context/auth/authContext';
 
-const Register = () => {
+const Register = props => {
 	const alertContext = useContext(AlertContext);
 	const authContext = useContext(AuthContext);
 
 	const { setAlert } = alertContext;
+	const { register, error, clearErrors, isAuthenticated } = authContext;
 
-	const { register } = authContext;
+	useEffect(() => {
+		if (isAuthenticated) {
+			props.history.push('/');
+		}
+
+		if (error === 'User already exists') {
+			setAlert(error, 'danger');
+			clearErrors();
+		}
+		// eslint-disable-next-line
+	}, [error, isAuthenticated, props.history]);
 
 	const [user, setUser] = useState({
 		name: '',
@@ -81,11 +92,12 @@ const Register = () => {
 						value={password2}
 						onChange={onChange}
 						required
+						minLength='6'
 					/>
 				</div>
 				<input
 					type='submit'
-					name='Register'
+					value='Register'
 					className='btn btn-primary btn-block'
 				/>
 			</form>

014883502ec1d3f7d2249ff8e2d8a2e48a330cea
varunswing <varuns939@gmail.com>
2020-04-30 23:42:31 +0530

    Update authReducer.js

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,7 +11,15 @@ import {
 
 export default (state, action) => {
 	switch (action.type) {
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				user: action.payload
+			};
 		case REGISTER_SUCCESS:
+		case LOGIN_SUCCESS:
 			localStorage.setItem('token', action.payload.token);
 			return {
 				...state,
@@ -20,6 +28,9 @@ export default (state, action) => {
 				loading: false
 			};
 		case REGISTER_FAIL:
+		case AUTH_ERROR:
+		case LOGIN_FAIL:
+		case LOGOUT:
 			localStorage.removeItem('token');
 			return {
 				...state,
@@ -29,5 +40,12 @@ export default (state, action) => {
 				user: null,
 				error: action.payload
 			};
+		case CLEAR_ERRORS:
+			return {
+				...state,
+				error: null
+			};
+		default:
+			return state;
 	}
 };

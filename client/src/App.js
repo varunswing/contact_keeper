56c9accfe792ba4ee35b073f6f729a86affe9ccf
varunswing <varuns939@gmail.com>
2020-04-30 23:41:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,17 @@ import About from './components/pages/About';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
 import Register from './components/auth/Register';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
 
 const App = () => {
 	return (
@@ -23,7 +29,11 @@ const App = () => {
 							<div className='container'>
 								<Alerts />
 								<Switch>
-									<Route exact path='/' component={Home} />
+									<PrivateRoute
+										exact
+										path='/'
+										component={Home}
+									/>
 									<Route
 										exact
 										path='/about'

b17225a5e9d138819822b0715949c67acfeb9269
varunswing <varuns939@gmail.com>
2020-04-30 23:42:35 +0530

    Update AuthState.js

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,7 +1,8 @@
 import React, { useReducer } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
 	REGISTER_SUCCESS,
 	REGISTER_FAIL,
@@ -25,7 +26,21 @@ const AuthState = props => {
 	const [state, dispatch] = useReducer(authReducer, initialState);
 
 	// Load User
-	const loadUser = () => console.log('loaduser');
+	const loadUser = async () => {
+		if (localStorage.token) {
+			setAuthToken(localStorage.token);
+		}
+		try {
+			const res = await axios.get('/api/auth');
+
+			dispatch({
+				type: USER_LOADED,
+				payload: res.data
+			});
+		} catch (err) {
+			dispatch({ type: AUTH_ERROR });
+		}
+	};
 
 	// Register User
 	const register = async formData => {
@@ -33,33 +48,55 @@ const AuthState = props => {
 			headers: {
 				'Content-Type': 'application/json'
 			}
-		}
+		};
 
 		try {
 			const res = await axios.post('/api/users', formData, config);
-	
+
 			dispatch({
 				type: REGISTER_SUCCESS,
 				payload: res.data
-			})
+			});
+
+			loadUser();
 		} catch (err) {
 			dispatch({
 				type: REGISTER_FAIL,
 				payload: err.response.data.msg
-			})
+			});
 		}
-	}
-
-	
+	};
 
 	// Login User
-	const login = () => console.log('login');
+	const login = async formData => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		try {
+			const res = await axios.post('/api/auth', formData, config);
+
+			dispatch({
+				type: LOGIN_SUCCESS,
+				payload: res.data
+			});
+
+			loadUser();
+		} catch (err) {
+			dispatch({
+				type: LOGIN_FAIL,
+				payload: err.response.data.msg
+			});
+		}
+	};
 
 	// Logout
-	const logout = () => console.log('logout');
+	const logout = () => dispatch({ type: LOGOUT });
 
 	// Clear Errors
-	const clearErrors = () => console.log('clearErrors');
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
 	return (
 		<AuthContext.Provider
